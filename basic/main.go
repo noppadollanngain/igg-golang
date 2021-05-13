b5322a6967e7afc8176b8a534e8ec8f6a4ed0f51
noppadol_la <noppadol@igeargeek.com>
2021-05-13 18:32:36 +0700

    [Edited] sss

--- basic/main.go
+++ basic/main.go
@@ -1,15 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeckFromFile("deck.txt")
+	cards.print()
+	cards.shuffle()
+	cards.print()
 }
